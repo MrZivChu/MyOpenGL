c2148cd7b8d6abb1574be7427a1dd1802776877a
mrzivchu <454719889@qq.com>
2023-04-08 10:56:45 +0800

    注释

--- MyOpenglDemo/MyOpenglDemo/OpenGL/texture.cpp
+++ MyOpenglDemo/MyOpenglDemo/OpenGL/texture.cpp
@@ -21,15 +21,15 @@ void Texture2D::Generate(GLuint width, GLuint height, unsigned char* data)
 {
 	this->Width = width;
 	this->Height = height;
-	// Create Texture
 	glBindTexture(GL_TEXTURE_2D, this->ID);
-	glTexImage2D(GL_TEXTURE_2D, 0, this->Internal_Format, width, height, 0, this->Image_Format, GL_UNSIGNED_BYTE, data);
-	// Set Texture wrap and filter modes
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, this->Wrap_S);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, this->Wrap_T);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, this->Filter_Min);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, this->Filter_Max);
-	// Unbind texture
+	// 上面只是创建一个Texture空壳，只是设置了相关属性，没有纹理数据
+	// 类似godot的TextureRect，纹理内容可以是如下几种类型：ImageTexture，ViewportTexture，CameraTexture，ExternalTexture，StreamTexture等
+	// glTexImage2D相当于为Texture赋了一个godot的ImageTexture类型，把读取的一张图片的char*数据赋给生成的Texture
+	glTexImage2D(GL_TEXTURE_2D, 0, this->Internal_Format, width, height, 0, this->Image_Format, GL_UNSIGNED_BYTE, data);
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
