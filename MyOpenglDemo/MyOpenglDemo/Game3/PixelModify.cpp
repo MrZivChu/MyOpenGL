a9c1bec9fc98eddda00a99e758a398a8e2beff85
mrzivchu <454719889@qq.com>
2023-04-08 10:38:20 +0800

    注释

--- MyOpenglDemo/MyOpenglDemo/Game3/PixelModify.cpp
+++ MyOpenglDemo/MyOpenglDemo/Game3/PixelModify.cpp
@@ -21,6 +21,8 @@ void PixelModify::Start()
 	//					2、绑定 Bind(id)  
 	//					3、设置属性
 	//					4、解绑 Bind(0)
+	// 使用的时候：
+	//					绑定 Bind(id)  [Shader除外，使用：glUseProgram(id)]
 
 	glGenVertexArrays(1, &planeVAO);
 	glBindVertexArray(planeVAO);
